05720f01319dade2c981df347c9181713bc395dc
Stewart Bryson <stewart@redpillanalytics.com>
2019-12-25 12:55:14 -0500

    Merged AnalyticsContainer and SinkContainer.

--- src/main/groovy/com/redpillanalytics/analytics/containers/SinkContainer.groovy
+++ src/main/groovy/com/redpillanalytics/analytics/containers/SinkContainer.groovy
@@ -7,13 +7,57 @@ import groovy.util.logging.Slf4j
  * Created by stewartbryson on 11/19/16.
  */
 @Slf4j
-@InheritConstructors
-class SinkContainer extends AnalyticsContainer {
+class SinkContainer {
 
    /**
     * The name of the container entity.
     */
    String name
+
+   /**
+    * The name of the container entity.
+    */
+   SinkContainer(String name) {
+
+      this.name = name
+   }
+
+   // capture the debug status
+   Boolean isDebugEnabled = log.isDebugEnabled()
+
+   /**
+    * Returns the container name.
+    *
+    * @return The container name.
+    */
+   def getDomainName() {
+
+      return ((getClass() =~ /\w+$/)[0] - "Container")
+   }
+
+   /**
+    * Log a debug message with the name of the container object.
+    */
+   void logTaskName(String task) {
+
+      log.debug "${getDomainName()}: $name, TaskName: $task"
+
+   }
+
+   /**
+    * Returns the container name.
+    *
+    * @return The container name.
+    */
+   def getTaskName(String baseTaskName) {
+
+      String taskName = name + baseTaskName.capitalize()
+
+      logTaskName(taskName)
+
+      return taskName
+
+   }
 
    String prefix, sink, suffix
 
@@ -26,7 +70,7 @@ class SinkContainer extends AnalyticsContainer {
    // Kafka properties
    String servers, serializerKey, serializerValue, acks, registry
 
-   Boolean ignoreErrors, formatSuffix=false
+   Boolean ignoreErrors, formatSuffix = false
 
    def getDescription() {
 
